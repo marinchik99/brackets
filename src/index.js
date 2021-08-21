879acd3af02a8a6a10238233136e0ceb077dd521
marinchik99 <marina_1999@tut.by>
2021-08-21 15:22:37 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let pairs = bracketsConfig.map((el) => el[0] + "" + el[1]);
+   let count = str.length;
+
+  while (count) {
+    for (let i=0; i<= pairs.length; i++) {
+      str = str.replace(pairs[i], "");
+    }
+    if (count === str.length) {
+      return false;
+    } 
+    count = str.length;
+  }
+
+  return true;
 }
